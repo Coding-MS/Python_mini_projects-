7ef02fd644eab08bf3a80c4620b52093e94b5165
Matthew  Smith <Matthew.smith102023@outlook.com>
2023-11-02 15:53:17 +0000

    Completed quiz project

---
+++ quiz_game.py
@@ -0,0 +1,48 @@
+print ("Welcome to my quiz!")
+
+playing = input("Do you want to play the game? ")
+score = 0 
+
+if playing,lower() != "yes": 
+    quit()
+
+print ("Okay! You have bravely taken the next step! Let the game begin!")
+
+answer = input ("What does CPU Stand for? ").lower()
+if answer == "central processing unit ":
+    print ("Correct")
+    score += 1
+else: 
+    print ("Answer is Incorrect!")
+    score -= 1
+
+
+answer = input ("What does RAM stand for? ").lower()
+if answer == "random access memory":
+    print ("Correct")
+    score += 1
+else: 
+    print ("Answer is Incorrect!")
+    score -= 1 
+
+
+answer = input ("What are animals with a backbone called? ").lower()
+if answer == "Vertebrate ":
+    print ("Correct")
+    score += 
+else: 
+    print ("Answer is Incorrect!")
+    score -= 1 
+
+
+answer = input ("What are Force is responsible keeping planets in orbit? ").lower()
+if answer == "gravity ":
+    print ("Correct")
+    print += 1
+else: 
+    print ("Answer is Incorrect!")
+    score -= 1 
+
+print ("You got " + str(score) + "questions correct!")
+print ("You got " + str((score / 4 )*100)+"%")
+
